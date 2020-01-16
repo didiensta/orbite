1b6f2fdfee3fbaa5f6389437d45cf4cc30afeb79
Thomas <warbeaver@pm.me>
2020-01-16 11:25:58 +0100

    much better binary data saving

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,21 @@
 
 #![allow(dead_code)]
 use ini::Ini;
-use std::{env::args, fs, io::ErrorKind};
+use std::fs::File;
 
 mod particules;
 mod tree;
 mod write;
 
-use crate::tree::*;
-use crate::write::*;
+use tree::Tree;
+use write::write_data_to_file;
 
-///Handles the whole simulation.
-fn simulation(tree: &mut Tree, time: f64, folder: String, crash_time: f64) {
-    //time
+//Handles the whole simulation.
+fn simulation(tree: &mut Tree, time: f64, mut file: &mut File, crash_time: f64) {
+    //Current time
     let mut t = 0f64;
-    //vector for infos
-    let mut infos = Vec::new();
-    //vector for inertia matrix
-    let mut inertia_matrices = Vec::new();
-    //count files
-    let mut c = 0;
+    //Iteration incrementor
+    let mut c = 0usize;
 
     //main loop
     while t < time {
@@ -45,23 +41,8 @@ fn simulation(tree: &mut Tree, time: f64, folder: String, crash_time: f64) {
         tree.compute_energy();
         tree.compute_epsilon();
         tree.compute_dt();
-
-        //save those values in vectors
-        infos.push(vec![
-            t,
-            tree.dynamical_time,
-            tree.energy,
-            tree.virial,
-            tree.rayons[0],
-            tree.rayons[1],
-            tree.rayons[2],
-        ]);
-        inertia_matrices.push(tree.inertia_matrix);
-
-        //write to file the positions of the particules and the density
-        write_positions(&tree, format!("{}/positions/{}.csv", folder, c.to_string()));
-
-        write_density(&tree, format!("{}/densities/{}.csv", folder, t.to_string()));
+        //Write saved data to file
+        write_data_to_file(t, c, tree, &mut file);
 
         //simulate 10 steps
         for _ in 0..10 {
@@ -73,81 +54,140 @@ fn simulation(tree: &mut Tree, time: f64, folder: String, crash_time: f64) {
 
         c += 1;
     }
-
-    //write all the values of infos and inertia_matrices to file
-    write_infos(&infos, &inertia_matrices, folder.clone());
 }
 
-fn read<T>(
-    section: &std::collections::HashMap<std::string::String, std::string::String>,
-    expr: &str,
-) -> T
-where
-    T: std::str::FromStr,
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
-{
-    //Yuck...
-    //Tring to factorise code here...
-    section.get(expr).unwrap().parse().unwrap()
+pub mod io {
+    //! Handles the I/O logic.
+
+    use std::env::args;
+    use std::fs::{create_dir, remove_dir_all};
+    use std::io::{stdin, stdout, ErrorKind, Write};
+
+    pub fn create_sim_folder(folder: &str) {
+        create_dir(folder).unwrap_or_else(|err| {
+            if err.kind() == ErrorKind::AlreadyExists {
+                println!(
+                    "Simulation folder already exists! \
+                     Do you whish to erase it? (y/_)"
+                );
+                let user_input = get_user_input_from_stdout();
+                if user_input == "y" {
+                    println!("Sure? Everything will be lost! (y/_)");
+                    let user_input = get_user_input_from_stdout();
+                    if user_input == "y" {
+                        println!("Erasing simulation folder...");
+                        remove_dir_all("sim").unwrap();
+                        create_dir("sim").unwrap();
+                    } else {
+                        println!("Exiting...");
+                        std::process::exit(1);
+                    }
+                } else {
+                    println!("Exiting...");
+                    std::process::exit(1);
+                }
+            } else {
+                panic!("Unforeseen error creating simulation folder.")
+            }
+        });
+    }
+
+    pub fn get_user_input_from_stdout() -> String {
+        //! Gets and cleans the user input from stdout
+
+        let _ = stdout().flush();
+        let mut user_input = String::new();
+
+        stdin()
+            .read_line(&mut user_input)
+            .expect("Couldn't read the file name");
+
+        //Remove potential \r's and \n's
+        loop {
+            if let Some('\n') = user_input.chars().next_back() {
+                user_input.pop();
+                continue;
+            } else if let Some('\r') = user_input.chars().next_back() {
+                user_input.pop();
+                continue;
+            }
+            break;
+        }
+
+        user_input
+    }
+
+    pub fn get_conf_file() -> String {
+        //! Tries to get the configuration file
+
+        if let Some(argument) = args().nth(1) {
+            //If an argument is given, try it.
+            argument
+        } else {
+            //Else, ask the user!
+            println!("Please enter a configuration file name:");
+
+            get_user_input_from_stdout()
+        }
+    }
+
+    pub fn read<T>(
+        section: &std::collections::HashMap<std::string::String, std::string::String>,
+        expr: &str,
+    ) -> T
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        //Yuck...
+        //Tring to factorise code here...
+        section.get(expr).unwrap().parse().unwrap()
+    }
 }
 
 fn main() {
     /////////////////////////////////////////////
     // read values from the configuration file //
     /////////////////////////////////////////////
-    let arg: String;
-    if let Some(argument) = args().nth(1) {
-        arg = argument
-    } else {
-        panic!("No configuration file given as argument!")
-    }
+    let arg = io::get_conf_file();
 
     let conf = Ini::load_from_file(format!("./{}", arg)).unwrap();
 
     let section = conf.section(None::<String>).unwrap();
 
     //number of particules
-    let nb_particules = read(section, "nb_particules");
+    let nb_particules = io::read(section, "nb_particules");
     //number of particles positions saved
-    let nb_particules_save = read(section, "nb_particules_save");
+    let nb_particules_save = io::read(section, "nb_particules_save");
     //dt = dynamycal_time / mu
-    let mu = read(section, "mu");
+    let mu = io::read(section, "mu");
     //epsilon = (4/(3*N*pi))^(1/3) R50 / lambda
-    let lambda = read(section, "lambda");
+    let lambda = io::read(section, "lambda");
     //initial value of the virial ratio
-    let virial: f64 = read(section, "virial");
+    let virial: f64 = io::read(section, "virial");
     //duration of the simulation in dynamical time
-    let time = read(section, "time");
+    let time = io::read(section, "time");
     //approximation of the acceleration
-    let theta = read(section, "theta");
+    let theta = io::read(section, "theta");
     //is it a plummer model or a uniform sphere
-    let plummer = read(section, "plummer");
+    let plummer = io::read(section, "plummer");
     //number of bins used for the density
-    let nb_bins = read(section, "nb_bins");
+    let nb_bins = io::read(section, "nb_bins");
     //number of neighbors used for the local density
-    let nb_neighbors = read(section, "nb_neighbors");
+    let nb_neighbors = io::read(section, "nb_neighbors");
     //folder name
     let folder = section.get("folder").unwrap();
     //we use special theta and mu for the start of the simulation
-    let crash_time = read(section, "crash_time");
-    let mu_init = read(section, "mu_init");
-    let theta_init = read(section, "theta_init");
+    let crash_time = io::read(section, "crash_time");
+    let mu_init = io::read(section, "mu_init");
+    let theta_init = io::read(section, "theta_init");
 
     //////////////////////////////////////////////
     /////////////// create folders ///////////////
     //////////////////////////////////////////////
-    fs::create_dir(folder.clone()).unwrap_or_else(|err| {
-        if err.kind() == ErrorKind::AlreadyExists {
-            panic!(
-                "Simulation file already exists! \
-                 Please change 'folder' in the configuration file."
-            )
-        } else {
-            panic!("Unforeseen error creating simulation folder")
-        }
-    });
-    fs::create_dir(format!("{}/positions", folder)).unwrap();
-    fs::create_dir(format!("{}/densities", folder)).unwrap();
+
+    io::create_sim_folder(folder);
+    let mut file = File::create("sim/data").unwrap();
 
     //////////////////////////////////////////////
     // build the octree and generate particules //
@@ -167,7 +207,8 @@ fn main() {
     );
 
     /////////////////////////////////////////////
-    //////////// run the simulations ////////////
+    //////////// run the simulation /////////////
     /////////////////////////////////////////////
-    simulation(&mut tree, time, folder.to_string(), crash_time);
+    println!("Starting the simulation");
+    simulation(&mut tree, time, &mut file, crash_time);
 }
--- src/write.rs
+++ src/write.rs
@@ -1,79 +1,64 @@
-use crate::tree::*;
-use rayon::prelude::*;
+use crate::tree::Tree;
+use bincode;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
 
-pub fn write_positions(tree: &Tree, file_name: String) {
-    let mut file = File::create(file_name).unwrap();
-    for i in 0..tree.nb_save {
-        write!(
-            &mut file,
-            "{};{};{};",
-            tree.particules[i].position[0] - tree.center[0],
-            tree.particules[i].position[1] - tree.center[1],
-            tree.particules[i].position[2] - tree.center[2]
-        )
-        .unwrap();
-        write!(
-            &mut file,
-            "{};{}\n",
-            tree.particules[i].cinetic,
-            tree.particules[i].mass * tree.particules[i].potential
-        )
-        .unwrap();
+#[derive(Serialize, Deserialize)]
+struct Data {
+    t: f64,
+    c: usize,
+    positions: Vec<[f64; 3]>,
+    speeds: Vec<[f64; 3]>,
+    rayons: [f64; 3],
+    inertia_matrix: [f64; 9],
+    energy: f64,
+    virial: f64,
+    dynamical_time: f64,
+    espilon: f64,
+}
+
+impl Data {
+    pub fn new(t: f64, c: usize, tree: &Tree) -> Data {
+        //! Create a new Data instance.
+        //! t is the current time, c the current iteration number,
+        //! and all other relevant data is *copied* from tree.
+
+        let mut positions = Vec::new();
+        let mut speeds = Vec::new();
+
+        for i in 0..tree.nb_save {
+            positions.push([
+                tree.particules[i].position[0] - tree.center[0],
+                tree.particules[i].position[1] - tree.center[1],
+                tree.particules[i].position[2] - tree.center[2],
+            ]);
+
+            speeds.push([
+                tree.particules[i].speed[0],
+                tree.particules[i].speed[1],
+                tree.particules[i].speed[2],
+            ])
+        }
+        Data {
+            t,
+            c,
+            positions,
+            speeds,
+            rayons: tree.rayons,
+            inertia_matrix: tree.inertia_matrix,
+            energy: tree.energy,
+            virial: tree.virial,
+            dynamical_time: tree.dynamical_time,
+            espilon: tree.epsilon,
+        }
     }
 }
 
-pub fn write_infos(infos: &[Vec<f64>], inertia_matrices: &[[f64; 9]], folder_name: String) {
-    let mut file = File::create(format!("{}/infos.csv", folder_name)).unwrap();
-    for info in infos.iter() {
-        for i in info {
-            write!(&mut file, "{};", i).unwrap();
-        }
-        writeln!(&mut file, "").unwrap();
-    }
-    let mut file_inertia = File::create(format!("{}/inertia_matrix.csv", folder_name)).unwrap();
-    for matrix in inertia_matrices.iter() {
-        for i in matrix {
-            write!(&mut file_inertia, "{};", i).unwrap();
-        }
-        writeln!(&mut file_inertia, "").unwrap();
-    }
-}
+pub fn write_data_to_file(t: f64, c: usize, tree: &Tree, file: &mut File) {
+    let data = Data::new(t, c, tree);
 
-//compute the density profile and then write it to file
-pub fn write_density(tree: &Tree, file_name: String) {
-    //compute and sort distances
-    let mut distances: Vec<f64> = tree
-        .particules
-        .par_iter()
-        .map(|p| {
-            f64::sqrt(
-                (0..3)
-                    .map(|i| (p.position[i] - tree.center[i]).powf(2f64))
-                    .sum(),
-            )
-        })
-        .collect();
-    distances.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let encoded_data: Vec<u8> = bincode::serialize(&data).unwrap();
 
-    let number_of_bins = tree.nb_bins;
-    let mut bins = vec![0f64; number_of_bins];
-    let mut bins_radii = vec![0f64; number_of_bins];
-    let bin_size = tree.particules.len() / number_of_bins;
-    let mut last_radius = 0f64;
-
-    //compute the density of each slices of bin_size particules
-    for i in 0..number_of_bins - 1 {
-        bins_radii[i] = distances[(i + 1) * bin_size];
-        let volume =
-            4. / 3. * std::f64::consts::PI * (bins_radii[i].powf(3f64) - last_radius.powf(3f64));
-        bins[i] = bin_size as f64 / volume / tree.particules.len() as f64;
-        last_radius = bins_radii[i];
-    }
-
-    let mut file = File::create(file_name).unwrap();
-    for i in 0..number_of_bins - 1 {
-        writeln!(&mut file, "{};{}", bins_radii[i], bins[i]).unwrap();
-    }
+    file.write(&encoded_data[..]).unwrap();
 }
